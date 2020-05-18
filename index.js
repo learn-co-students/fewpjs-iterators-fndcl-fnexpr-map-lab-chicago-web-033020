0626389d6c10ac8c97c0ab6413135b9f16e6d2c6
doodleman218 <dcwolff218@gmail.com>
2020-05-18 10:04:15 -0500

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const titleCased = (input) => {
     return tutorials.map( tutorial => {  //why does this need return
     let newLine = tutorial.split(' ')
     let newCap = 
-    newLine.map( line => line[0].toUpperCase() + line.slice(1)  ) //what is slice doing here
+    newLine.map( line => line[0].toUpperCase() + line.slice(1)  ) //why is slice here
     console.log(newLine)  //why does this not show up with uppercase
     let response = newCap.join(' ')
     return response
